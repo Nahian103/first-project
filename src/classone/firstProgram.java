0880600d75e5995f8159f40a791a56b16640a5aa
Nahian103 <106032958+Nahian103@users.noreply.github.com>
2022-06-12 10:14:10 -0400

    2ndday

---
+++ src/classone/firstProgram.java
@@ -0,0 +1,16 @@
+package classone;
+
+public class firstProgram {
+	// comments is all always after those two lines
+	public static void main(String[] args) {
+		System.out.println("Hello NYC");
+		System.out.println("the Big Apple");
+		String myname = "nahian";
+		int age = 32;
+		int amount = 20 ;
+		double percantage = 8;
+		double value = amount*percantage/100;
+		System.out.println(value);
+
+	}
+}
